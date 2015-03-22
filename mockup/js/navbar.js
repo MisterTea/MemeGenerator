80ded5867971bc3d88091975065db70c2f370fda
Jason Gauci <jgauci@apple.com>
2015-03-22 13:27:20 -0700

    Mock-up

---
+++ mockup/js/navbar.js
@@ -0,0 +1,4 @@
+app.controller('NavbarController', ['$scope', '$rootScope', 'retryHttp', '$modal', '$timeout', function($scope, $rootScope, retryHttp, $modal, $timeout) {
+  $scope.myself = null;
+  $scope.projectName = "Tidal Wave";
+}]);
